4f2082eb543c2a44c392679ab03d4f6d570bfb0a
Tio <t6babayemi@gmail.com>
2023-05-31 19:22:34 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project endeavours to predict combat results that feature prominently in Dragon Ball Z, known for its wide-ranging fan base. Utilising machine learning tools, the research will analyse critical factors that could greatly influence these outcomes including participating characters' identities and strengths as well as their relevant locations within a given saga period.',
+    author='Tio Babayemi',
+    license='MIT',
+)
